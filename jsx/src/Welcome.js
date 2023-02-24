12acdcd6081e61b826c9974f15e679aaa003a579
Matteo Spinelli <matteospinelli10@gmail.com>
2023-02-24 11:06:20 +0100

    add exercise files

--- jsx/src/Welcome.js
+++ jsx/src/Welcome.js
@@ -1,7 +1,12 @@
 import React from "react";
 
-export class Welcome extends React.Component{
-    render(){
-        return <p>Welcome, {this.props.name ? this.props.name : "User"}!</p>
+export class Welcome extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>Welcome, {this.props.name ? this.props.name : "User"}!</p>
+                <p>Your age is {this.props.age}</p>
+            </div>
+        )
     }
 }
