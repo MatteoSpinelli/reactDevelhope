357da5dbb78fd5f95c05f0d20bfb333af21d6f47
Matteo Spinelli <matteospinelli10@gmail.com>
2023-02-24 09:46:32 +0100

    add exercise files

--- jsx/src/Hello.js
+++ jsx/src/Hello.js
@@ -1,12 +1,10 @@
 import React from "react";
-import { Message } from "./Message"
 
 export class Hello extends React.Component{
     render(){
         return (
             <div>
                 <h1>Hello, World!</h1>
-                <Message />
             </div>
         )
     }
