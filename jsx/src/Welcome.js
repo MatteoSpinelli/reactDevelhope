2d8ff73a0ad34f74179282fd68cac27617f8d0cf
Matteo Spinelli <matteospinelli10@gmail.com>
2023-02-24 11:11:34 +0100

    add exercise files

--- jsx/src/Welcome.js
+++ jsx/src/Welcome.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { Age } from "./Age";
 
 export class Welcome extends React.Component {
     render() {
         return (
             <div>
                 <p>Welcome, {this.props.name ? this.props.name : "User"}!</p>
-                <p>Your age is {this.props.age}</p>
+                <Age age={this.props.age} />
             </div>
         )
     }
