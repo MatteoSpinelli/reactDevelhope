064cfab56b024a74b94ec4c2a5444711cfa2db61
Matteo Spinelli <matteospinelli10@gmail.com>
2023-03-02 12:01:09 +0100

    som

--- jsx/src/App.js
+++ jsx/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Hello } from "./Hello"
 import { Welcome } from "./Welcome";
-import { ClickTracker } from "./ClickTracker";
 
 export class App extends React.Component{
     render(){
@@ -9,7 +8,6 @@ export class App extends React.Component{
             <div>
                 <Hello />
                 <Welcome name="John" age="21" />
-                <ClickTracker  />
             </div>
         )
     }
