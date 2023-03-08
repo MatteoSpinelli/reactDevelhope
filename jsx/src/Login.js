a914de7880bcef0d44a4ea1c761b14a854aa9818
Matteo Spinelli <matteospinelli10@gmail.com>
2023-03-08 21:51:46 +0100

    fix

--- jsx/src/Login.js
+++ jsx/src/Login.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export class Login extends React.Component{
+export class Login extends React.Component {
 
     state = {
         username: "",
@@ -21,26 +21,27 @@ export class Login extends React.Component{
     }
 
 
-    render(){
+    render() {
         return (
             <form>
                 <for>Username:</for>
-                <input type="text" name = "username" value={this.state.username} onChange={this.handleChange}/>
+                <input type="text" name="username" value={this.state.username} onChange={this.handleChange} />
                 <for>Password:</for>
-                <input type="password" name = "password" value={this.state.password} onChange={this.handleChange}/>
+                <input type="password" name="password" value={this.state.password} onChange={this.handleChange} />
                 <for>Remember:</for>
-                <input type="checkbox" name = "remember" checked={this.state.remember} onChange={this.handleChange}/>
-                <br/>
-                <button disabled={this.controlButton()} onClick={
-                    (event) => {
-                        event.preventDefault()
-                        this.props.onlogin(this.state)
-                    }
-                } style={
-                    {
-                        backgroundColor: this.state.password.length <= 8 ? "red": "green"
-                    }
-                }>Login</button>
+                <input type="checkbox" name="remember" checked={this.state.remember} onChange={this.handleChange} />
+                <br />
+                <button disabled={this.controlButton()}
+                    onClick={
+                        (event) => {
+                            event.preventDefault()
+                            this.props.onlogin(this.state)
+                        }
+                    } style={
+                        {
+                            backgroundColor: this.state.password.length <= 8 ? "red" : "green"
+                        }
+                    }>Login</button>
             </form>
         )
     }
