6766600866fd1fa87b1070ef8b4a0f9280c44e3a
Matteo Spinelli <matteospinelli10@gmail.com>
2023-03-15 19:21:48 +0100

    ex

--- jsx/src/App.js
+++ jsx/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Hello } from "./Hello"
 import { Welcome } from "./Welcome";
 import { Counter } from "./Counter";
+import { GithubUser } from "./GithubUser";
 
 export class App extends React.Component{
     render(){
@@ -10,6 +11,7 @@ export class App extends React.Component{
                 <Hello />
                 <Welcome name="John" age="21" />
                 <Counter />
+                <GithubUser username = "MatteoSpinelli"/>
             </div>
         )
     }
