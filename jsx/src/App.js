a7302828224c1a892291ec2d974460fb31a8be49
Matteo Spinelli <matteospinelli10@gmail.com>
2023-03-15 19:06:29 +0100

    ex

--- jsx/src/App.js
+++ jsx/src/App.js
@@ -3,13 +3,17 @@ import { ClickCounter } from "./ClickCounter";
 import { Hello } from "./Hello"
 import { Welcome } from "./Welcome";
 
-export class App extends React.Component{
-    render(){
+export class App extends React.Component {
+    state = {
+        ren: true
+    }
+
+    render() {
         return (
             <div>
                 <Hello />
                 <Welcome name="John" age="21" />
-                <ClickCounter initialValue={15} />
+                <ClickCounter initialValue={15} onCounterChange={(counter) => console.log(counter)} />
             </div>
         )
     }
--- jsx/src/ClickCounter.js
+++ jsx/src/ClickCounter.js
@@ -1,10 +1,15 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
-export function ClickCounter({ initialValue = 0 }){
+export function ClickCounter({ initialValue = 0, onCounterChange }) {
     let [counter, setCounter] = useState(initialValue)
-    function handleClickCounter(){
+    function handleClickCounter() {
         setCounter(c => c + 1)
     }
+
+    useEffect(() => {
+        onCounterChange(counter)
+    }, [counter, onCounterChange])
+
     return (
         <div>
             <h1>{counter}</h1>
