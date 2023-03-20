3a52ad1f6899d8559c003503e2b8875e0426e782
Matteo Spinelli <matteospinelli10@gmail.com>
2023-03-20 11:11:14 +0100

    add exercise

--- jsx/src/App.js
+++ jsx/src/App.js
@@ -3,13 +3,17 @@ import { Hello } from "./Hello"
 import { Welcome } from "./Welcome";
 import { Counter } from "./Counter";
 import { Form } from "./Form";
-import { Route, Routes } from "react-router-dom"
+import { Link, Route, Routes } from "react-router-dom"
 
 export function App() {
     return (
         <div className="container">
+            <div>
+                <Link to="/">Home</Link> | <Link to="/counter">Counter</Link>
+            </div>
             <Routes>
                 <Route path="/" element={<Welcome name="Matteo" age={21} />} />
+                <Route path="/counter" element={<Counter />} />
             </Routes>
         </div>
     )
