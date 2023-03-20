476ffb20f3dd93fef1cce2f2d7ad004891fbb188
Matteo Spinelli <matteospinelli10@gmail.com>
2023-03-20 11:45:40 +0100

    efweff

--- jsx/src/DisplayLanguage.js
+++ jsx/src/DisplayLanguage.js
@@ -1,15 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 import { LanguageContext } from "./LanguageContentx";
 
-export class DisplayLanguage extends React.Component {
-    render() {
-        return (
-            <LanguageContext.Consumer>
-                {(value) => {
-                    return <h1>The language is: {value}</h1>
-                }}
-            </LanguageContext.Consumer>
-        )
+export function DisplayLanguage() {
+    const lang = useContext(LanguageContext)
+    return (
 
-    }
+        <h1>The language is: {lang}</h1>
+
+    )
+
 }
