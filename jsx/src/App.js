2850fbaf4a8b2d10c6c53b3107794600a2f0cf4f
Matteo Spinelli <matteospinelli10@gmail.com>
2023-03-20 11:16:09 +0100

    add exercise

--- jsx/src/App.js
+++ jsx/src/App.js
@@ -4,16 +4,18 @@ import { Welcome } from "./Welcome";
 import { Counter } from "./Counter";
 import { Form } from "./Form";
 import { Link, Route, Routes } from "react-router-dom"
+import { ShowGithubUser } from "./ShowGithubUser";
 
 export function App() {
     return (
         <div className="container">
             <div>
-                <Link to="/">Home</Link> | <Link to="/counter">Counter</Link>
+                <Link to="/">Home</Link> | <Link to="/counter">Counter</Link> | <Link to="/users/MatteoSpinelli">User</Link>
             </div>
             <Routes>
                 <Route path="/" element={<Welcome name="Matteo" age={21} />} />
                 <Route path="/counter" element={<Counter />} />
+                <Route path="/users/:username" element={<ShowGithubUser />} />
             </Routes>
         </div>
     )
---
+++ jsx/src/GithubUser.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react"
+
+export function GithubUser({ username = "MatteoSpinelli" }){
+    let [data, setData] = useState({})
+
+    async function fetchDataGit(username){
+        const res = await fetch(`https://api.github.com/users/${username}`)
+        if (res.status === 200){
+            const json = await res.json()
+            setData(json)
+        }
+    }
+
+    useEffect(() => {
+        fetchDataGit(username)
+    }, [username])
+
+    return (
+        <div>
+            <h1>{data.name}</h1>
+            <img src={data.avatar_url} alt="avatar"/>
+        </div>
+    )
+}
