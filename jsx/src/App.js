517c69a452687987df7969f82570e852d32f1521
Matteo Spinelli <matteospinelli10@gmail.com>
2023-03-20 11:37:04 +0100

    add exercise

--- jsx/src/App.js
+++ jsx/src/App.js
@@ -6,17 +6,20 @@ import { Form } from "./Form";
 import { Link, Route, Routes } from "react-router-dom"
 import { ShowGithubUser } from "./ShowGithubUser";
 import { NotFound } from "./NotFound";
+import { GithubUserList } from "./GithubUserList";
 
 export function App() {
     return (
         <div className="container">
             <div>
-                <Link to="/">Home</Link> | <Link to="/counter">Counter</Link> | <Link to="/users/MatteoSpinelli">User</Link>
+                <Link to="/">Home</Link> | <Link to="/counter">Counter</Link> | <Link to="/users">User</Link>
             </div>
             <Routes>
                 <Route path="/" element={<Welcome name="Matteo" age={21} />} />
                 <Route path="/counter" element={<Counter />} />
-                <Route path="/users/:username" element={<ShowGithubUser />} />
+                <Route path="/users" element={<GithubUserList />}>
+                    <Route path=":username" element={<ShowGithubUser />} />
+                </Route>
                 <Route path="*" element={<NotFound />} />
             </Routes>
         </div>
---
+++ jsx/src/GithubUserList.js
@@ -0,0 +1,31 @@
+import { useState } from "react"
+import { Link, Outlet } from "react-router-dom"
+import { GithubUser } from "./GithubUser"
+
+export function GithubUserList(){
+    let [userList, setUserList] = useState([])
+    let [username, setUsername] = useState("")
+    
+
+    function handleChange(evt){
+        setUsername(evt.target.value)
+    }
+
+    function handleAddList(){
+        setUserList((userList) => {
+            console.log([...userList, username])
+            return [...userList, username]
+        })
+    }
+
+    return (
+        <div>
+            <input type="text" name="username" value={username} onChange={handleChange}/>
+            <button onClick={handleAddList}>Add</button>
+            {userList.map((user) => {
+                return <Link key={user} to={`/users/${user}`}>{user}</Link>
+            })}
+            <Outlet />
+        </div>
+    )
+}
