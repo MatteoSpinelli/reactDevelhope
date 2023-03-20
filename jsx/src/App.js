3627d46dff5088957a63daf8ac52911de31265fa
Matteo Spinelli <matteospinelli10@gmail.com>
2023-03-20 12:20:22 +0100

    add exercise

--- jsx/src/App.js
+++ jsx/src/App.js
@@ -7,6 +7,37 @@ import { Link, Route, Routes } from "react-router-dom"
 import { ShowGithubUser } from "./ShowGithubUser";
 import { NotFound } from "./NotFound";
 import { GithubUserList } from "./GithubUserList";
+import { FilteredList } from "./FilteredList";
+
+
+const objs = [
+    {
+        name: "matteo",
+        id: 1,
+        age: 21
+    },
+    {
+        name: "giorgio",
+        id: 2,
+        age: 1
+    },
+
+    {
+        name: "olivia",
+        id: 3,
+        age: 19
+    },
+    {
+        name: "asia",
+        id: 4,
+        age: 20
+    },
+    {
+        name: "marco",
+        id: 5,
+        age: 12
+    }
+]
 
 export function App() {
     return (
@@ -23,6 +54,8 @@ export function App() {
                 </Route>
                 <Route path="*" element={<NotFound />} />
             </Routes>
+
+            <FilteredList objs={objs} />
         </div>
     )
 }
--- jsx/src/FilteredList.js
+++ jsx/src/FilteredList.js
@@ -1,3 +1,15 @@
-export function FilteredList({ onjs }){
+import { useMemo } from "react";
 
+
+export function FilteredList({ objs }) {
+    const filteredValues = useMemo(() => {
+        return objs.filter((obj) => obj.age >= 18)
+    }, [objs])
+    return (
+        <div>
+            {filteredValues.map((el) => {
+                return <h1 key={el.id}>{el.name}, {el.age}</h1>
+            })}
+        </div>
+    )
 }
