80f72216995963210d3945dff1fa821d3face37a
Matteo Spinelli <matteospinelli10@gmail.com>
2023-03-21 10:36:34 +0100

    add ex

--- jsx/src/useGithubUser.js
+++ jsx/src/useGithubUser.js
@@ -4,7 +4,7 @@ import useSWR from "swr"
 const fetcher = url => fetch(url).then(res => res.json())
 
 export function useGithubUser(username){
-    const {data, error, mutate} = useSWR(`https://api.github.com/users/${username}`, fetcher)
+    const {data, error, mutate} = useSWR(username ? `https://api.github.com/users/${username}` : null, fetcher)
 
     return {
         user: data,
