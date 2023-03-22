0af5ee2f3ccb1f300e51079e3a08dd5e7f598b81
Matteo Spinelli <matteospinelli10@gmail.com>
2023-03-22 13:48:30 +0100

    ex

--- jsx/src/App.js
+++ jsx/src/App.js
@@ -3,15 +3,17 @@ import { Hello } from "./Hello"
 import { Welcome } from "./Welcome";
 import { Counter } from "./Counter";
 import { GithubUser } from "./GithubUser";
+import { Pokedex } from "./Pokedex";
 
 export class App extends React.Component{
     render(){
         return (
             <div>
-                <Hello />
+                {/* <Hello />
                 <Welcome name="John" age="21" />
                 <Counter />
-                <GithubUser username = ""/>
+                <GithubUser username = ""/> */}
+                <Pokedex />
             </div>
         )
     }
